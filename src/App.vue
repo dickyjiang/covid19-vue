a42c55faff1400490d83c4d9c8463d3ba73d0c12
dicky <dickyjuwono@gmail.com>
2020-03-15 02:57:53 +0700

    1st

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="h-screen w-full flex-col items-center">
+    <div class="flex items-center justify-between bg-white shadow-md mx-4">
+      <div class="flex flex-1 bg-gray-300 max-w-screen-lg items-center">
+          <h3 class="text-3xl font-black tracking-tight leading-none text-gray-700 text-left self-start">
+          Novel
+          <span class="block">
+          Coronavirus
+          </span>
+          <span class="block text-3xl font-light">
+          COVID-19
+          </span>
+          </h3>
+      </div>
+
+      <div>
+        <nav class="text-base text-gray-600 font-semibold">
+          <button class="mx-4">
+          Halaman Depan
+          </button>
+          <button class="mx-4">
+          Informasi
+          </button>
+          <button class="mx-4">
+          Tentang Kami
+          </button>
+          
+        </nav>
+      </div>
+    </div>
+
+    <main>
+      <div >
+        <h1 style="font-family: 'Shrikhand', display;" class="text-5xl text-left leading-none">
+        Bersama Kita Bisa !
+        <span class="block text-left text-red-700 text-4xl">
+
+        #lawancorona
+        </span>
+        </h1>
+
+      </div>
+    </main>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
 </script>
 
 <style>
@@ -24,5 +59,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  @import url('https://fonts.googleapis.com/css?family=Shrikhand&display=swap');
 }
 </style>
