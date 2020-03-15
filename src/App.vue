43e5bf006354e35fbe19c180748fd88709592d14
dicky <dickyjuwono@gmail.com>
2020-03-15 11:47:22 +0700

    2nd

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,43 @@
 <template>
-  <div id="app" class="h-screen w-full flex-col items-center">
-    <div class="flex items-center justify-between bg-white shadow-md mx-4">
-      <div class="flex flex-1 bg-gray-300 max-w-screen-lg items-center">
-          <h3 class="text-3xl font-black tracking-tight leading-none text-gray-700 text-left self-start">
-          Novel
-          <span class="block">
-          Coronavirus
-          </span>
-          <span class="block text-3xl font-light">
-          COVID-19
-          </span>
-          </h3>
-      </div>
-
+  <div id="app" class="">
+    <header class="flex justify-between items-center bg-white py-3 px-12 shadow-lg">
       <div>
-        <nav class="text-base text-gray-600 font-semibold">
-          <button class="mx-4">
-          Halaman Depan
-          </button>
-          <button class="mx-4">
-          Informasi
-          </button>
-          <button class="mx-4">
-          Tentang Kami
-          </button>
-          
-        </nav>
+              <h3 class="text-2xl font-black tracking-tight leading-none text-gray-700 text-left">
+              Novel
+              <span class="block">
+              Coronavirus
+              </span>
+              <span class="block text-2xl font-light">
+              COVID-19
+              </span>
+              </h3>
       </div>
-    </div>
+      <div>
+          <div class="text-base text-gray-600 font-semibold">
+              <button class="mx-4">
+              Halaman Depan
+              </button>
+              <button class="mx-4">
+              Informasi
+              </button>
+              <button class="mx-4">
+              Tentang Kami
+              </button>
+          </div>
+      </div>
+    </header>
 
-    <main>
-      <div >
-        <h1 style="font-family: 'Shrikhand', display;" class="text-5xl text-left leading-none">
+
+    <div class="flex-1 bg-gray-200">
+      <div class="h-screen max-w-screen-lg mx-auto pt-12 px-12">
+        <h1 style="font-family:'Shrikhand', display;" class="text-5xl text-left leading-none">
         Bersama Kita Bisa !
         <span class="block text-left text-red-700 text-4xl">
-
         #lawancorona
         </span>
         </h1>
-
       </div>
-    </main>
+    </div>
 
 
   </div>
@@ -53,12 +50,11 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Barlow, Shrikhand, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-  @import url('https://fonts.googleapis.com/css?family=Shrikhand&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Barlow|Barlow+Condensed|Barlow+Semi+Condensed|Shrikhand&display=swap');
 }
 </style>
