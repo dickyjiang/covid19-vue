bcd338ab27f16a8f657b40c00b2a2063b7aeae35
dicky <dickyjuwono@gmail.com>
2020-03-16 23:27:21 +0700

    implement new layout to master

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="max-h-screen flex flex-col">
     <header
       class="flex justify-between items-end bg-white py-3 px-2 md:px-12 shadow-lg"
     >
@@ -31,129 +31,183 @@
       </div>
     </header>
 
-    <div class="flex-1 bg-gray-200 container mx-auto pt-8 min-w-full h-screen">
-      <div
-        class="relative max-w-screen-lg h-3/4 container mx-auto object-contain pt-24 pb-12 px-12"
-      >
-        
-        <div class="absolute top-0 left-0 pt-2 md:pt-4 pl-8 md:pl-8">
-          <h1
-            style="font-family:'Shrikhand', display;"
-            class="text-2xl md:text-5xl text-left leading-none"
-          >
-            Bersama Kita Bisa !
-            <span class="block text-left text-red-700 text-xl md:text-4xl">
-              #lawancorona
-            </span>
-          </h1>
-        </div>
-      </div>
-      <div
-        class="container mx-auto w-full h-auto md:w-3/4 py-2"
-      >
+    <div class="flex flex-1 bg-gray-700">
+      <!-- sidepanel left -->
+      <div class="w-1/4 bg-beige pl-4 pr-12">
         <div
-          class="tableauPlaceholder"
-          id="viz1584294563043"
-          style="position: relative; max-width: 100%;"
+          class="text-4xl font-extrabold text-bg-gray-700 text-left leading-10 pl-4 pt-4 mt-4"
         >
-          <noscript
-            ><a href="#"
-              ><img
-                alt=" "
-                src="https://public.tableau.com/static/images/Pe/PetaSebaranODPPDP-BlueVersion/DashboardMapAllCases/1_rss.png"
-                style="border: none"/></a></noscript
-          ><object class="tableauViz" style="display:none;"
-            ><param name="host_url" value="https://public.tableau.com/"/>
-            <param name="embed_code_version" value="3"/>
-            <param
-              name="path"
-              value="views/PetaSebaranODPPDP-BlueVersion/DashboardMapAllCases?:embed=y&amp;:display_count=y"/>
-            <param name="toolbar" value="no"/><param
-              name="static_image"
-              value="https://public.tableau.com/static/images/Pe/PetaSebaranODPPDP-BlueVersion/DashboardMapAllCases/1.png"/>
-            <param name="animate_transition" value="yes"/><param
-              name="display_static_image"
-              value="yes"/><param name="display_spinner" value="yes"/><param
-              name="display_overlay"
-              value="yes"/><param name="display_count" value="yes"
-          /></object>
+          BERSAMA
+          <div class="block">
+            KITA
+          </div>
+          BISA
         </div>
-      </div>
-      <div
-        class="container mx-auto bg-white h-48 w-4/5 md:w-3/4 rounded-lg shadow-xl py-2 px-4"
-      >
-        <div class="text-2xl font-black text-center pt-4 pb-8">
-        Cari Lokasi Rumah Sakit Rujukan Terdekat
+        <div
+          style="font-family:'Shrikhand', display;"
+          class="text-left text-red-700 text-2xl pl-4"
+        >
+          #lawancorona
         </div>
-        <div class="flex justify-around items-center">
-          <div class="flex-initial font-semibold text-white bg-red-700 w-1/4 py-3 px-3 m-1 rounded-lg shadow-md">
-          Provinsi
+        <div class="flex flex-col items-start pl-4">
+          <div class="text-base font-bold text-gray-700 mt-4">
+            Cari Lokasi
           </div>
-          <div class="flex-initial font-semibold text-white bg-red-700 w-1/4 py-3 px-3 m-1 rounded-lg shadow-md">
-          Kota / Kabupaten
-          </div>
-          <div class="flex-initial font-semibold text-white bg-red-700 w-1/4 py-3 px-3 m-1 rounded-lg shadow-md">
-          Kecamatan
-          </div>
-          <div class="flex-initial font-semibold text-white bg-red-700 w-1/4 py-3 px-3 m-1 rounded-lg shadow-md">
-          Kelurahan / Desa
-          </div>
+          <input
+            class="w-3/4 p-2 rounded-md mt-2"
+            type="search"
+            name="lokasi"
+            id="lokasi"
+          />
         </div>
       </div>
 
-      <div
-        class="flex container mx-auto w-3/4 justify-evenly flex-stretch mt-12 "
-      >
-        <div class=" w-2/6 ">
-          <H2
-            style="font-family:'Barlow Condensed', sans-serif;"
-            class="m-2 text-xl font-bold text-left"
-          >
-            Apa itu corona virus ?
-          </H2>
-          <p class="text-left pr-8 border-r-2 border-gray-400">
-            Coronavirus merupakan keluarga besar virus yang menyebabkan penyakit
-            ringan sampai berat, seperti common cold atau pilek dan penyakit
-            yang serius seperti MERS dan SARS - Penularannya dari hewan ke
-            manusia (zoonosis) dan penularan dari manusia ke manusia sangat
-            terbatas.
-          </p>
+      <!-- middle section -->
+      <div class="flex flex-col flex-1 min-h-full justify-between">
+        <!-- map section   -->
+        <div class="flex-1">
+          <div class="container mx-auto">
+            <div
+              class="tableauPlaceholder"
+              id="viz1584294563043"
+              style="position: relative; max-width: 100%;"
+            >
+              <noscript
+                ><a href="#"
+                  ><img
+                  class="object-contain h-64 w-full"
+                    alt=" "
+                    src="https://public.tableau.com/static/images/Pe/PetaSebaranODPPDP-BlueVersion/DashboardMapAllCases/1_rss.png"
+                    style="border: none"
+                /></a> </noscript
+              ><object class="tableauViz" style="display:none;"
+                ><param name="host_url" value="https://public.tableau.com/"/>
+                <param name="embed_code_version" value="3"/>
+                <param
+                  name="path"
+                  value="views/PetaSebaranODPPDP-BlueVersion/DashboardMapAllCases?:embed=y&amp;:display_count=y"/>
+                <param name="toolbar" value="no"/><param
+                  name="static_image"
+                  value="https://public.tableau.com/static/images/Pe/PetaSebaranODPPDP-BlueVersion/DashboardMapAllCases/1.png"/>
+                <param name="animate_transition" value="yes"/><param
+                  name="display_static_image"
+                  value="yes"/><param name="display_spinner" value="yes"/><param
+                  name="display_overlay"
+                  value="yes"/><param name="display_count" value="yes"
+              /></object>
+            </div>
+          </div>
         </div>
-        <div class="w-2/6">
-          col 2
+        <!-- graph section -->
+        <div class="flex-initial h-64 bg-gray-600">
+          graph
         </div>
-        <div class="w-2/6 bg-gray-400">
-          Twitter column
+      </div>
+
+      <div class="flex flex-col w-1/4 bg-gray-300">
+        <div class="px-1 pb-8">
+          <div class="text-left font-bold text-xl mb-3 bg-gray-500 p-3">
+            Data
+          </div>
+          <div class="mb-2">
+            <div class="text-base font-bold w-full text-left mb-2">
+              Provinsi
+              <div class="text-lg font-semibold">
+                'Data Provinsi'
+              </div>
+            </div>
+            <div class="text-base font-bold w-full text-left mb-2">
+              Kota / Kabupaten
+              <div class="text-lg font-semibold">
+                'Data Kota'
+              </div>
+            </div>
+            <div class="text-base font-semibold w-full text-left mb-2">
+              Kecamatan
+              <div class="text-lg font-semibold">
+                'Data Kecamatan'
+              </div>
+            </div>
+            <div class="text-base font-semibold w-full text-left mb-2">
+              Kelurahan / Desa
+              <div class="text-lg font-semibold">
+                'Data Desa'
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-gray-500 mt-2">
+            <div class="text-base font-semibold w-full text-left">
+              Status
+            </div>
+            <div class="text-base font-semibold w-full text-left">
+              Gender
+            </div>
+            <div class="text-base font-semibold w-full text-left">
+              Usia
+            </div>
+          </div>
+        </div>
+
+        <div class="flex-1 bg-red-600">
+          <div class="w-full mt-2 p-2">
+            <header
+              class="text-lg text-white text-left font-semibold pb-2 mb-2 border-b bordeer-gray-100"
+            >
+              Latest Tweet
+            </header>
+
+            <div class=" h-full overflow-y-scroll">
+              <div
+                class="mt-3 border px-3 py-5 bg-gray-800 rounded-md shadow-md flex flex-start"
+              >
+                <div
+                  class="rounded-full bg-gray-200 h-10 w-10 mr-4 flex-shrink-0"
+                >
+                  pic
+                </div>
+                <div class="flex flex-col flex-1 items-start">
+                  <div class="text-gray-200 font-bold text-left text-xl">
+                    Twitter account
+                  </div>
+                  <p class="text-gray-300 text-xl mb-2 leading-tight truncate">
+                    sample of long tweets form user
+                  </p>
+                  <p
+                    class="text-gray-400 fonts-base leading-tight break-all text-left"
+                  >
+                    #lawancorona #covid19 #corona
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-
-    <!-- <div class="h-screen bg-blue-200">
-      section 2
-    </div> -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  mounted () {
-    var divElement = document.getElementById('viz1584294563043');
-    var vizElement = divElement.getElementsByTagName('object')[0];
+  name: "app",
+  mounted() {
+    var divElement = document.getElementById("viz1584294563043");
+    var vizElement = divElement.getElementsByTagName("object")[0];
     if (divElement.offsetWidth > 800) {
-      vizElement.style.width = '1366px';
-      vizElement.style.height = '1366px';
+      vizElement.style.width = "1366px";
+      vizElement.style.height = "1366px";
     } else if (divElement.offsetWidth > 500) {
-      vizElement.style.width = '100%';
-      vizElement.style.height = '1200px';
+      vizElement.style.width = "100%";
+      vizElement.style.height = "1200px";
     } else {
-      vizElement.style.width = '100%';
-      vizElement.style.height = '1200px';
+      vizElement.style.width = "100%";
+      vizElement.style.height = "1200px";
     }
-    var scriptElement = document.createElement('script');
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    var scriptElement = document.createElement("script");
+    scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
     vizElement.parentNode.insertBefore(scriptElement, vizElement);
-  },
+  }
 };
 </script>
 
